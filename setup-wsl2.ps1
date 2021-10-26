de2e4e62b7847aa4f352fe24f22e1f37efe395ce
Alexander Pajer <alexander.pajer@integrations.at>
2021-10-26 06:50:07 +0200

    wsl

--- setup-wsl2.ps1
+++ setup-wsl2.ps1
@@ -1,5 +1,20 @@
+function Update-Kernel () {
+    Write-Host(" ...Downloading WSL2 Kernel Update.")
+    $kernelURI = 'https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi'
+    $kernelUpdate = ((Get-Location).Path) + '\wsl_update_x64.msi'
+    (New-Object System.Net.WebClient).DownloadFile($kernelURI, $kernelUpdate)
+    Write-Host(" ...Installing WSL2 Kernel Update.")
+    msiexec /i $kernelUpdate /qn
+    Start-Sleep -Seconds 5
+    Write-Host(" ...Cleaning up Kernel Update installer.")
+    Remove-Item -Path $kernelUpdate
+}
+
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+Update-Kernel
 wsl --set-default-version 2
 choco install wsl-ubuntu-2004 -y
+choco install docker-desktop -y
+
